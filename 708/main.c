1dba7955c675151ecfb3358b904b70cac92ace1c
justapig9020 <justapig9020@gmail.com>
2020-04-19 19:51:32 +0800

    807 still with bugs

--- 708/main.c
+++ 708/main.c
@@ -6,38 +6,71 @@ struct Node {
     struct Node *next;
 };
 
-void insert(struct Node *ptr, int val)
+void insert(struct Node **ptr, int val)
 {
-    ptr = malloc (sizeof(struct Node));
-    printf ("new: %p\n", ptr);
-    ptr->val = val;
-    ptr->next = NULL; 
+    *ptr = malloc (sizeof(struct Node));
+    (*ptr)->val = val;
+    (*ptr)->next = NULL; 
 }
 
 void prt_list(struct Node *head)
 {
-    struct Node *ptr;
-    ptr = head;
-    printf ("\nPrint List: ");
-    while (ptr->next != head) {
-        printf ("%d, ", ptr->val);
-        ptr = ptr->next;
-    }
-    printf ("%d\n", ptr->val);
-}
-
-void insertVal(struct Node *head, int val)
-{
     struct Node **ptr;
-    ptr = &head;
-    if (*ptr) {
-        while (*ptr != head) {
+    printf ("Print List: ");
+    if (head) {
+        ptr = &head;
+        while ((*ptr)->next != head) {
+            printf ("%d, ", (*ptr)->val);
             ptr = &((*ptr)->next);
         }
+        printf ("%d\n", (*ptr)->val);
     }
-    *ptr = malloc (sizeof(struct Node));
-    (*ptr)->val = val;
-    (*ptr)->next = head;
+}
+
+struct Node *insertVal(struct Node *head, int val)
+{
+    struct Node *ret;
+    struct Node *ptr;
+    struct Node *run;
+    int buf;
+
+    if (head) {
+        ptr = head;
+        buf = ptr->val;
+        run = malloc (sizeof(struct Node));
+        ret = run;
+        run->val = ptr->val;
+        ptr = ptr->next;
+
+        while (ptr != head) {
+            if (buf > ptr->val || 
+                    (buf <= val && ptr->val >val) ) {
+                run->next = malloc (sizeof(struct Node));
+                run = run->next;
+                run->val = val;
+                run->next = NULL;
+            }
+            run->next = malloc (sizeof(struct Node));
+            run = run->next;
+            run->val = ptr->val;
+            run->next = NULL;
+            buf = ptr->val;
+            ptr = ptr->next;
+        }
+        if ((buf > ptr->val && !((buf > val)^(ptr->val > val)))|| 
+                (buf <= val && ptr->val >val) ) {
+            run->next = malloc (sizeof(struct Node));
+            run = run->next;
+            run->val = val;
+            run->next = NULL;
+        }
+        run->next = ret;
+    } else {
+        ret = malloc (sizeof(struct Node));
+        ret->val = val;
+        ret->next = ret;   
+    }
+    return ret;
 }
 
 int main(void)
@@ -46,26 +79,24 @@ int main(void)
     int val;
     struct Node *head;
     struct Node **ptr;
+    struct Node *ret;
     printf ("list amount: ");
     scanf ("%d", &n);
     
+    head = NULL;
     ptr = &head;
     for (int i=0; i<n; i++) {
         scanf ("%d", &val);
-        printf ("%p\n", *ptr);
-        insert (*ptr, val);
-        printf ("%p\n", *ptr);
+        insert (ptr, val);
         ptr = &((*ptr)->next);
     }
-    printf ("out!\n");
-    //*ptr = head;
-    prt_list(head);
-
+    *ptr = head;
+    prt_list (head);
 
     printf ("Insert val: ");
     scanf ("%d", &val);
-    insertVal (head, val);
-    prt_list(head);
+    ret = insertVal (head, val);
+    prt_list (ret);
 
     return 0;
 }
